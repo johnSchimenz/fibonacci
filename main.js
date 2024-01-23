d9d57d21c69cb46aafa0e24dce4726e490dbe77e
John Schimenz <john.schimenz@gmail.com>
2024-01-22 22:36:10 -0600

    STarted on fibsRec function, but having difficulty with the reursive portion

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function fibs(number) {
     } else {
         let array = [0, 1];
         for (i = 2; i < number; i++) {
-            let newNumber = array[i-2] + array [i-1];
+            let newNumber = array[i-2] + array[i-1];
             array.push(newNumber);
         }
         return array;
@@ -22,3 +22,27 @@ console.log(fibs(3));
 console.log(fibs(4));
 console.log(fibs(5));
 console.log(fibs(8));
+
+function fibsRec(number) {
+    if (number == 1) {
+        return [0];
+    } else if (number == 2) {
+        return [0, 1];
+    } else if (number < 1) {
+        return "This number doesn't work";
+    } else if (number == 3) {  
+        let baseArray = [0, 1];
+        let newestNumber = Number(baseArray[number - 2]) + Number(baseArray[number - 1]);     
+        return baseArray.concat(newestNumber);
+    }
+}
+
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(2));
+console.log(fibsRec(3));
+console.log(fibsRec(4));
+/*
+console.log(fibsRec(5));
+console.log(fibsRec(8));
+*/
