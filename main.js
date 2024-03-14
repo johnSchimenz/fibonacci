ec7f229e7696de7e6d6b15e8751ae14280678c56
John Schimenz <john.schimenz@gmail.com>
2024-03-13 21:42:00 -0500

    test case

--- main.js
+++ main.js
@@ -113,3 +113,4 @@ console.log(mergeSort([55, 88, -101]));
 console.log(mergeSort([-9. -81, -5]));
 console.log(mergeSort([5, -1, -20]));
 console.log(mergeSort([5, -2, 10]));
+console.log(mergeSort([9, 81, 729]));
